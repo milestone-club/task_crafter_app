8456a69858086dd7c9ce76c65d93cb688d95c6f2
Lasyamithra <151613556+Lasyamithra@users.noreply.github.com>
2024-08-22 18:28:26 +0530

    Delete lib directory

--- lib/widgets/constant.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color kYellowLight = Color(0xFFFFF7EC);
-const Color kYellowDark = Color.fromARGB(255, 235, 182, 117);
-const Color kYellow = Color(0xFFFAF0DA);
-
-const Color kBlueLight = Color(0xFFEDF4FE);
-const Color kBlueDark = Color(0xFFFFF7EC);
